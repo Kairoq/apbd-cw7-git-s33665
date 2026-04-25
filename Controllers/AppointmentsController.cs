42f695d0da5f11f020adca537e4a7709fe3b485c
Kairoq <maksik771@wp.pl>
2026-04-25 19:48:56 +0200

    Added Web extension to the project. Manually added missing appsettings.json. Added AppointmentsController.

---
+++ Controllers/AppointmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using ConsoleApp1.DTOs;
+
+namespace ConsoleApp1.Controllers;
+
+[Route("api/[controller]")] 
+[ApiController]
+public class AppointmentsController : ControllerBase
+{
+    private readonly string _connectionString;
+
+    public AppointmentsController(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection") 
+                            ?? throw new InvalidOperationException("Connection string not found");
+    }
+}
